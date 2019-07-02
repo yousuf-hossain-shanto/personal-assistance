b4fe8eed7ccfbac13f949bdad34cb521c5be3202
Yousuf Hossain <yousuf.hossain.shanto@gmail.com>
2019-07-03 00:25:46 +0600

    Download option for database

--- routes/web.php
+++ routes/web.php
@@ -32,18 +32,35 @@ Route::group(['prefix' => 'education', 'as' => 'education.'], function () {
 });
 
 Route::group(['prefix' => 'automation', 'as' => 'automation.'], function () {
-   Route::get('gmail-imap', 'AutomationController@gmailImap')->name('gmail-imap');
+    Route::get('gmail-imap', 'AutomationController@gmailImap')->name('gmail-imap');
 
-   Route::get('queue-work', function () {
-       return Illuminate\Support\Facades\Artisan::call('queue:work', ['--stop-when-empty' => true]);
-   });
+    Route::get('queue-work', function () {
+        return Illuminate\Support\Facades\Artisan::call('queue:work', ['--stop-when-empty' => true]);
+    });
 
-   Route::get('inspect/{table}/{action?}', function ($table, $action = 'm') {
-       $q = \Illuminate\Support\Facades\DB::table($table);
-       if ($action == 'ddl') {
-           return $q->delete();
-       }
-       return $q->get();
-   });
+    Route::get('inspect/{table}/{action?}', function ($table, $action = 'm') {
+        $q = \Illuminate\Support\Facades\DB::table($table);
+        if ($action == 'ddl') {
+            return $q->delete();
+        }
+        return $q->get();
+    });
+
+    Route::get('dump-db-yt', function () {
+        $file = './../database/database.sqlite';
+
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . basename($file));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        ob_clean();
+        flush();
+        readfile($file);
+        exit;
+    });
 
 });
